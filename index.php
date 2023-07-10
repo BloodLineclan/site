e7b7ac0fc6a4d5c91a6f2fe6a687706cafec885b
BloodLineclan <139109729+BloodLineclan@users.noreply.github.com>
2023-07-10 10:55:30 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Мой сайт</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <img class="header-image" src="/path/to/header-image.jpg" alt="Заголовок">
+
+    <div class="buttons">
+      <a href="your-clan-link" class="button">Вступить в клан</a>
+      <button onclick="toggleForm()" class="button">Подать заявку на союз</button>
+    </div>
+
+    <div id="form-container" class="form-container">
+      <form action="submit.php" method="post">
+        <label for="clan">Ваш клан:</label>
+        <input type="text" name="clan" id="clan" required>
+
+        <label for="clan-leader">Ссылка на главу клана:</label>
+        <input type="text" name="clan-leader" id="clan-leader" required>
+
+        <label for="alliance">Выберите тип союза:</label>
+        <input type="radio" name="alliance" value="Нейтралитет" required> Нейтралитет
+        <input type="radio" name="alliance" value="Союз" required> Союз
+
+        <input type="submit" value="Отправить">
+      </form>
+    </div>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
