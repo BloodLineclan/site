0cd9d65b64b42d0c6674a54b7409dadacc567528
BloodLineclan <139109729+BloodLineclan@users.noreply.github.com>
2023-07-10 12:16:54 +0300

    Update sumbit.php

--- sumbit.php
+++ sumbit.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Заявка отправлена</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-  <div class="container">
-    <h1>Заявка отправлена!</h1>
-  </div>
-</body>
-</html>
+<?php
+// Получение отправленных данных
+$clan = $_POST['clan'];
+$leader = $_POST['leader'];
+$status = $_POST['status'];
+
+// Сохранение данных в базе SQLite
+$db = new SQLite3('data.db'); // Замените "data.db" на путь к вашей реальной базе данных
+$db->exec("CREATE TABLE IF NOT EXISTS applications (clan TEXT, leader TEXT, status TEXT)");
+$stmt = $db->prepare("INSERT INTO applications (clan, leader, status) VALUES (:clan, :leader, :status)");
+$stmt->bindValue(':clan', $clan, SQLITE3_TEXT);
+$stmt->bindValue(':leader', $leader, SQLITE3_TEXT);
+$stmt->bindValue(':status', $status, SQLITE3_TEXT);
+$stmt->execute();
+
+// Закрытие базы данных
+$db->close();
+
+// Перенаправление обратно на главную страницу
+header('Location: index.html');
+?>
