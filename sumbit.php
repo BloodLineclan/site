9b0513f972aea2076b2c3cc8b54b4bc65122a2b4
BloodLineclan <139109729+BloodLineclan@users.noreply.github.com>
2023-07-10 10:57:39 +0300

    Create sumbit.php

---
+++ sumbit.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Заявка отправлена</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Заявка отправлена!</h1>
+  </div>
+</body>
+</html>
